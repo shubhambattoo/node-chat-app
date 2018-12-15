3ae3163f84594928d9e3fb46108a76867ffaf268
shubhambattoo <shubham.battoo@hotmail.com>
2018-12-15 17:53:54 +0530

    added moment to format time and show it

--- public/js/index.js
+++ public/js/index.js
@@ -9,21 +9,23 @@ socket.on('disconnect', () => {
 })
 
 socket.on('newMessage', (message) => {
-	console.log('New Message');
-	console.log(message);
-
+	// console.log('New Message');
+	// console.log(message);
+  const formattedTime = moment(message.createdAt).format('h:mm a');
+  // console.log(formattedTime)
 	const li = $('<li></li>');
-	li.text(`${message.from} : ${message.text}`);
+	li.text(`${message.from} ${formattedTime} : ${message.text}`);
 
 	$('#messages').append(li);
 
 });
 
 socket.on('newLocationMessage', (message) => {
+  const formattedTime = moment(message.createdAt).format('h:mm a');
   const li = $('<li></li>');
   const a = $('<a target="_blank">My current location</a>');
 
-  li.text(`${message.from}: `);
+  li.text(`${message.from} ${formattedTime}: `);
   a.attr('href', message.url);
 
   li.append(a);
